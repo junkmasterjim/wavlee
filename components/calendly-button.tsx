eed0234b359c60992d44ad3de29f3f4debbd4d57
Noah Pittman <noahpittman00@gmail.com>
2024-03-19 15:35:16 -0400

    Refactor CalendlyButton component

--- components/calendly-button.tsx
+++ components/calendly-button.tsx
@@ -14,18 +14,18 @@ const CalendlyButton = () => {
 	return (
 		<>
 			{root ? (
-				<div
+				<Button
+					asChild
+					variant={"outline"}
+					size={"sm"}
 					className="md:text-base border-muted/50 bg-muted/10 backdrop-blur-sm inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 border hover:bg-accent hover:text-accent-foreground h-9 px-3"
-					onClick={() => {
-						console.log(root);
-					}}
 				>
 					<PopupButton
 						url="https://calendly.com/prodbywavlee"
 						rootElement={root}
 						text="Book a Session"
 					/>
-				</div>
+				</Button>
 			) : (
 				<Button
 					disabled
